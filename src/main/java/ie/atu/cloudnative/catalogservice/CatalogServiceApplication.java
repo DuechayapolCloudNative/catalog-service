55b3c70c4e660a73350caaa37e9dc1b72e4000b9
duechayapolgmit <duechayapolsbs@gmail.com>
2026-01-02 19:58:36 +0000

    dummy change

--- src/main/java/ie/atu/cloudnative/catalogservice/CatalogServiceApplication.java
+++ src/main/java/ie/atu/cloudnative/catalogservice/CatalogServiceApplication.java
@@ -15,7 +15,7 @@ public class CatalogServiceApplication {
 	private Environment environment;
 
 	public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Hello");
 		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
 }
